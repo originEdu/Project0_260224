14a54660e9a89eee837ce3cd522b1d49af3c512a
originEdu <originpark82@gmail.com>
2026-02-24 15:00:44 +0900

    SnakeGame 소스 코드 추가
    ai가 만들어준 SnakeGame 소스 코드 추가

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
-#include<iostream>
-
-int main()
-{
-	std::cout << "Hello World!" << std::endl;
-	std::cout << 1*2*3*4 << std::endl;
-	return 0;
-}
+//#include<iostream>
+//
+//int main()
+//{
+//	std::cout << "Hello World!" << std::endl;
+//	std::cout << 1*2*3*4 << std::endl;
+//
+//	return 0;
+//}
